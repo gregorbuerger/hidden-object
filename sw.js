27d2c35acaaac796f14837a35d2720d9782ccff8
gregorbuerger <gregor.buerger@googlemail.com>
2026-09-22 10:39:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const C='hidden-object-v04';
-const A=['./','index.html','style.css','app.js','manifest.webmanifest','scene-game.png','auto.png','hut.png','lupe.png','schluessel.png','kompass.png','clean-auto.png','clean-hut.png','clean-lupe.png','clean-schluessel.png','clean-kompass.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='hidden-v05';const F=['./','index.html','style.css','app.js','manifest.webmanifest','scene-clean.png','auto.png','hut.png','lupe.png','schluessel.png','kompass.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
